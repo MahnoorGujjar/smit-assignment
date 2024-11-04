f97a17c7c098aea90d791496abaaafd6352e5082
Mahnoor Gujjar <160156566+MahnoorGujjar@users.noreply.github.com>
2024-11-04 16:59:19 +0500

    Add files via upload

---
+++ assignment/chap2.js
@@ -0,0 +1,86 @@
+// CHAPTER 2
+
+// QUESTION 1 
+
+// 1. Declare a variable called username.
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+// 3. Write script to a) Declare a JS variable, titled message. b) Assign “Hello World” to variable message c) Display the message in alert box.
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes. 
+
+// let username;
+
+// let myName = "mahnoor";
+
+// let message = "Hello World";
+
+// alert(message);
+
+// let studentName = "Ali";
+// let studentAge = 25;
+// let studentGrade = 'A';
+
+// alert("Name :"+studentName);
+// alert("Age :"+studentAge);
+// alert("Grade :"+studentGrade);
+
+
+
+
+//.............................................................................................................................................................
+
+
+
+// QUESTION 2
+
+
+// 5. Write a script to display the following alert using one JS variable: 
+
+// alert("\N PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+//.............................................................................................................................................................
+
+
+
+// QUESTION 3
+
+// 6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use string concatenation) 
+
+
+// let message = "my E-mail address is"
+
+// let email = " @mahanooray1@gail.com"
+
+// alert(message + email)
+
+//.............................................................................................................................................................
+
+// QUESTION 4
+
+//7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box: 
+
+// let book = "A smarter way to learn JavaScript"
+
+// alert(book)
+
+//................................................................................................................................................................
+
+
+// QUESTION 5
+
+
+// 8. Write a script to display this in browser through JS 
+
+
+// let message= "Yah! I can write HTML content through javascript";
+// document.write(message);
+
+
+
+//...................................................................................................................................................................
+
+// QUESTION 6
+
+// 9. Store following string in a variable and show in alert and browser through JS 
+
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ")
---
+++ assignment/chap3.js
@@ -0,0 +1,47 @@
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box. 
+
+
+// alert("i am 20 years old")
+
+//........................................................................................................................................................
+
+
+// 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page.
+//  Show his/her number of visits on your web page. For example: “You have visited this site N times”. 
+
+// alert("you have visited this site 13 times")
+
+
+
+//.........................................................................................................................................................
+
+
+
+// 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser: 
+
+// let birthYear = "my birth year is 1998"
+
+// document.write(birthYear + " <br> data type of my declared variable is string")
+
+
+//...........................................................................................................................................................
+
+
+
+
+//  4. A visitor visits an online clothing store www.xyzClothing.com . 
+//  Write a script to store in variables the following information:
+//  b. Product title c. Quantity i.e. how many products a visitor wants to order Show the following message in your browser:
+//  “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”. 
+
+
+// let customerName = "John Doe";
+
+// let Product = "T'shirts";
+
+// let Quantity = 7;
+
+// let message = customerName + " " + 'ordered' + " " + Quantity + " " + Product + " " + "from" + " " + "xyzClothing.com"
+
+// document.write(message)
+
