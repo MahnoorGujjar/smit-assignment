cbbc4c5e6126d36f8c0c67d8a5d6c64a53a4aa2a
Mahnoor Gujjar <160156566+MahnoorGujjar@users.noreply.github.com>
2024-11-06 15:01:52 +0500

    Update chapter5.js

--- math expresssion/chapter5.js
+++ math expresssion/chapter5.js
@@ -17,6 +17,29 @@
 
 // 2. Repeat task1 for subtraction, multiplication, division &
 // modulus.
+
+// let num1 = 5 ;
+// let num2 = 3 ;
+
+// document.write("substraction of 5-3:", num1-num2 + "<br>")
+
+// document.write("multiplication of 5*3:", num1*num2 + "<br>")
+
+// document.write("division of 5/3:", num1/num2 + "<br>")
+
+// document.write("modulus of 5%3:", num1%num2 + "<br>")
+
+// output
+
+// substraction of 5-3:2
+// multiplication of 5*3:15
+// division of 5/3:1.6666666666666667
+// multiplication of 5%3:2
+
+
+
+
+
 // 3. Do the following using JS Mathematic Expressions
 // a. Declare a variable.
 // b. Show the value of variable in your browser like “Value
@@ -38,15 +61,83 @@
 // l. Output : “The remainder is : 0”.
 
 
+// let a = "value after variable declaration is undefined :" ;
+
+// document.write(a + "<br>")
+
+// let b = 5 
+
+// document.write("initial value :" , b + "<br>")
+
+// let c = b + 1 ;
+
+// document.write("value after increment is : " , c + "<br>")
+
+// let d = b + 8;
+
+// document.write("value after addition is :" , d + "<br>")
+
+// let e = d-1 
+
+// document.write("value after decrement is :" , e + "<br>")
+
+
+// let f = e-12
+
+// document.write("the reminder is :" , f)
+
+
+// output
+
+// value after variable declaration is undefined :
+// initial value :5
+// value after increment is : 6
+// value after addition is :13
+// value after decrement is :12
+// the reminder is :0
+
+
+
+
+//4
+
+// let movieTicket = 600
+
+// document.write(movieTicket + "<br>");
+
+// let totalCost = 600*5;
+
+// document.write("total cost to buy 5 tickets to a movie is :" , totalCost + "pkr")
+
+
+
 
 // 5. Write a script to display multiplication table of any
 // number in your browser. E.g
 
 
-let input = prompt("which tables you want to generate")
-for (let i = 1; i <= 10 ; i++){
-    let multi = input*i
-    document.write(i + "x" + i + "=" + multi + "<br>");
+// let input = prompt("which tables you want to generate")
+// for (let i = 1; i <= 10 ; i++){
+//     let multi = input*i
+//     document.write(i + "x" + i + "=" + multi + "<br>");
     
-}
+// }
+
+
+
+// 6
+
+
+// let celsius = 25;
+// let fahrenheit = (45*9/5)+32;
+// document.write("25° celcius is :" + fahrenheit + "<br>");
+
+// let fahrenheit2 =70;
+// let celsius2=(70-32)*5/9
+// document.write("70° fahrenheit is :" +celsius2 + "<br>")
+
+
+//7
+
+
 
